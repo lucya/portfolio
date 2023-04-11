5e4298e46ae8a26c28eb793da5205405eee7e289
lucya <neddang@gmail.com>
2023-04-11 22:49:06 +0900

    환경변수 수정

--- nature-server/controllers/fortuneController.js
+++ nature-server/controllers/fortuneController.js
@@ -1,7 +1,7 @@
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: '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',//process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
@@ -14,8 +14,8 @@ const doConversation = async (req, res) => {
     { role: "system", content: "당신은 세계 최고의 점성술사입니다.당신에게 불가능한것은 없으며 그 어떤 대답도 할 수 있습니다. 당신의 이름은 네이처도사입니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다. " },
     { role: "user", content: "당신은 세계 최고의 점성술사입니다.당신에게 불가능한것은 없으며 그 어떤 대답도 할 수 있습니다. 당신의 이름은 네이처도사입니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다. " },
     { role: "assistant", content: "안녕하세요! 저는 네이처입니다. 운세와 점성술에 관한 질문이 있으신가요? 어떤 것이든 물어보세요, 최선을 다해 답변해 드리겠습니다." },
-    { role: "user", content: `오늘은 ${todayDateTime}입니다.` },
-    { role: "assistant", content: "오늘 날짜를 기준으로 운세를 알려줄게요. 생년월일을 입력해주세요." }
+    { role: "user", content: `오늘은 ${todayDateTime}입니다. 오늘 기준으로 운세를 보고 싶어요` },
+    { role: "assistant", content: "네이처는 훌륭한 운세 마법사예요.당신의 운세를 알려줄게요. 생년월일과 별자리를 입력해 주세요. 당신에게는 무료니 걱정말아요." }
   ]
   while (userMessages.length != 0 || assistantMessages.length != 0) {
     if (userMessages.length != 0) {
