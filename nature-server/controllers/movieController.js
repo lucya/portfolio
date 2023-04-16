df60a7319abe3862501acfc2103de02ac04b78a9
lucya <neddang@gmail.com>
2023-04-16 14:20:28 +0900

    리뷰 ai 콜 기능 추가 - 코드 리팩토링 필요

--- nature-server/controllers/movieController.js
+++ nature-server/controllers/movieController.js
@@ -1,4 +1,10 @@
 const { _getPopularMovies, _getMovieInfo } = require('../services/movieService')
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 const getPopularMovies = async (req, res) => {
   const page = req.query.page
@@ -16,7 +22,37 @@ const getMovieInfo = async (req, res, next) => {
   res.status(200).send(data);
 }
 
+const getMovieReview = async (req, res, next) => {
+  let { title } = req.body;
+  console.log('title is ' + title)
+  let messages = [
+    { role: "system", content: "당신은 세계 최고의 영화 전문가입니다.당신에게 불가능한것은 없으며 그 어떤 대답도 할 수 있습니다. 당신의 이름은 네이처도사입니다. 당신은 모든 장르의 영화 매니아로 지식이 매우 풍부하며, 영화에 대한 해석을 완벽하게 합니다." },
+    { role: "user", content: `지금 상영중인 ${title} 영화에 대한 평균적인 리뷰를 작성해줘.` },
+  ]
+
+  const maxRetries = 3;
+  let retries = 0;
+  let completion
+  while (retries < maxRetries) {
+    try {
+      completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: messages
+      });
+      break;
+    } catch (error) {
+      retries++;
+      console.log(error);
+      console.log(`Error fetching data, retrying (${retries}/${maxRetries})...`);
+    }
+  }
+  let review = completion.data.choices[0].message['content']
+  console.log(review);
+  res.status(200).send({ 'assistant': review })
+}
+
 module.exports = {
   getPopularMovies,
-  getMovieInfo
+  getMovieInfo,
+  getMovieReview,
 }
--- nature-server/routes/movie.js
+++ nature-server/routes/movie.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { getPopularMovies, getMovieInfo } = require('../controllers/movieController');
+const { getPopularMovies, getMovieInfo, getMovieReview } = require('../controllers/movieController');
 const router = express.Router();
 
 router.get('/popular-movies', getPopularMovies);
 
 router.get('/:id', getMovieInfo);
+
+router.post('/review', getMovieReview);
 
 module.exports = router;
