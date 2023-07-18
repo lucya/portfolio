bd657df6f20dce155025b6e1a84f01ba884feab1
lucya <neddang@gmail.com>
2023-07-18 21:22:55 +0900

    영화 목록 페이징 버그 수정 및 예고편 reducer 추가

--- nature-web/src/reducers/movieSlice.ts
+++ nature-web/src/reducers/movieSlice.ts
@@ -1,17 +1,21 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { Movie, MovieType } from '../actions/movie/types';
+import { createSlice, createSelector } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+import { Movie, MovieType, MovieVideoType } from '../actions/movie/types';
 import { PURGE } from "redux-persist";
+import { RootState } from '../app/store';
 
 interface MovieState {
   movies?: Array<MovieType> | undefined;
   movie?: MovieType | null | undefined;
   page?: number;
+  videos?: Array<MovieVideoType> | undefined;
 }
 
 const initialState: MovieState = {
   movies: [],
   movie: null,
-  page: 1
+  page: 1,
+  videos: [],
 }
 const movieSlice = createSlice({
   name: 'movie',
@@ -19,7 +23,10 @@ const movieSlice = createSlice({
   reducers: {
     //...state와 자동 return
     getMovies: (state, action: PayloadAction<MovieState>) => {
-      const { movies, page } = action.payload
+      let { movies, page } = action.payload
+      if (!movies || !movies.length) {
+        movies = [];
+      }
       state.movies = [...state.movies as [], ...movies as []]
       state.page = page
       state.movie = Movie
@@ -29,8 +36,13 @@ const movieSlice = createSlice({
       console.log('Movie', movie)
       state.movie = movie
     },
+    getMovieVideos: (state, action: PayloadAction<MovieState>) => {
+      const { videos } = action.payload
+      state.videos = videos
+    },
     initMovie: (state, action: PayloadAction) => {
       state.movie = Movie
+      state.videos = []
     },
   },
   //초기화하고 싶은 state가 있는 slice마다 아래를 추가해야한다.
@@ -38,5 +50,6 @@ const movieSlice = createSlice({
     builder.addCase(PURGE, () => initialState);
   }
 })
+
 export const movieActions = movieSlice.actions
 export default movieSlice.reducer;
