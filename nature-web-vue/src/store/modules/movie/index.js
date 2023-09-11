6ed8509949c490a094ae8f2a7ac9ff73a12c6af4
lucya <neddang@gmail.com>
2023-09-11 23:10:46 +0900

    api 분리 및 호출 변경

--- nature-web-vue/src/store/modules/movie/index.js
+++ nature-web-vue/src/store/modules/movie/index.js
@@ -1,3 +1,5 @@
+import { getMoviesApi } from '@/api/movie'
+
 export default {
   namespaced: true,
   state: {
@@ -8,28 +10,31 @@ export default {
     addMovies(state, payload) {
       state.movies = [...state.movies, ...payload]
     },
-    initMovies(state, payload) {
+    initMovies(state) {
       state.movies = []
       state.page = 0
     },
-    setPage(state, payload) {
-      state.page += 1
+    setPage(state) {
+      state.page++
     }
   },
-  getters: {
-    
-  },
+  getters: {},
   actions: {
-    
-    SET_PAGE({ state, commit }) {
-      commit('setPage')
-    },
-    ADD_MOVIES({ state, commit }, payload) {
-      commit('addMovies', payload)
-    },
-    INIT_MOVIES({ state, commit }) {
+    initMovies({ commit }) {
       commit('initMovies')
-    }
-  },
+    },
+    async getMovies({ state, commit }) {
+      try {
+        commit('setPage')
+        // await http.get('/api/movie/popular-movies', { params: { page: state.page } })
+        await getMoviesApi(state.page)
+          .then(({ data }) => {
+            commit('addMovies', data)
+          })
+      } catch (error) {
+        throw new Error(`API ${error}`)
+      }
+    },
 
+  },
 }
